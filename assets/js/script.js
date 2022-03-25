ccd5502803f900ffcfff01f9c04d2a1eeb3f33c9
Deenie818 <99102981+Deenie818@users.noreply.github.com>
2022-03-24 19:42:02 -0700

    addressed usability concerns

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
-
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
@@ -13,14 +12,16 @@ var taskFormHandler = function(event) {
     type: taskTypeInput
   };
 
+  //check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
+
   //send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
-
-  
-
-  
-  
-  
 };
 
 var createTaskEl = function(taskDataObj) {
